cfcf994eb4978d7ba0379664707a474f34790794
Ayothegod <heyayomideadebisi@gmail.com>
2022-11-12 08:27:14 +0100

    created jwt sign, onto creating its middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,19 @@
 const User = require('../models/User.js')
 const {BadRequestError} = require('../errors')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const {StatusCodes} = require('http-status-codes')
 
 const register = async (req,res) => {
     const {name,email,password} = req.body
-
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password,salt)
-
     const temperUser = {name,email,password:hashedPassword}
 
     const user = await User.create({...temperUser})
-    // if(req.body.email == User.email) return res.send('email exist')
-    res.status(StatusCodes.CREATED).json({  user })
+    const token = jwt.sign({userId:user._id, name:user.name},"jaiasi92",{expiresIn:'30d'})
+    // console.log(token);
+    res.status(StatusCodes.CREATED).json({ user:{name:user.name},token })
 }
 const login = async (req,res) => {
     res.send(req.body)
