eebaee13d11e25e9ab1ffb9775405f47d61f7538
Ayothegod <heyayomideadebisi@gmail.com>
2022-11-13 06:31:47 +0100

    Added expres  security packages -> xss,cors,helmet,ate-limit etc

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const connectDB  = require('./db/connect.js')
 
+//security
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 //routes
 const logRouter = require('./routes/auth.js')
 const Router = require('./routes/jobs.js')
@@ -13,8 +19,16 @@ const auth = require('./middleware/authentication.js')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max:100,
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 // routes
 app.use('/api/v1/auth', logRouter);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,13 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
+
+  let customError = {
+    //set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'something went wrong, try again later'
+  }
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
